c5b9dad3e49250e35323bc49079f2e8f9ef1236d
Barry Tabios <b_tabios@yahoo.com>
2017-04-29 14:51:03 +1000

    initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,23 @@
+class PagesController < ApplicationController
+  def home
+    # file = File.read('melbourne_fishing_spots.json')
+    # @search = JSON.parse(file)
+    #
+    # @search.each do |place|
+    #   location = Location.new
+    #   location.name = place['Location']
+    #   location.fish = place['Fish']
+    #   location.latitude = place['Latitude']
+    #   location.longitude = place['Longitude']
+    #   location.save
+    # end
+    @search = Location.all
+    render :home
+  end
+
+  def show
+    @spot = Location.find(params[:id])
+    render :show
+  end
+
+end
